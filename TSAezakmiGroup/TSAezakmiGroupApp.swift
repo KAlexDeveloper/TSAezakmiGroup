da6d6c2f3010da0ab8de88f492c461b2b964f286
son-dev-a <anderhander4@gmail.com>
2025-04-22 19:16:22 +0300

    Initial Commit

---
+++ TSAezakmiGroup/TSAezakmiGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  TSAezakmiGroupApp.swift
+//  TSAezakmiGroup
+//
+//  Created by сонный on 22.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct TSAezakmiGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
